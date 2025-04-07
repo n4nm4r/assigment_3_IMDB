f493b0189de293cbfc000fcfe3e738c886f14709
n4nm4r <nancy.martin.nb@gmail.com>
2025-04-07 10:42:04 -0300

    Add project files.

---
+++ assigment_3_IMDB/Models/Generated/Genre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace assigment_3_IMDB.Models;
+
+public partial class Genre
+{
+    public int GenreId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Title> Titles { get; set; } = new List<Title>();
+}
